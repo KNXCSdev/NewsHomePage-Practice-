ef93f9a2a013324500b7fd4707c88b40c3aebb3f
KNXCSdev <michal.miksho@gmail.com>
2024-10-09 19:38:34 +0200

    JS changed

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const menuOpen = document.querySelector(".icon-menu");
 const menu = document.querySelector(".menu");
-const menuClose = document.querySelector(".close-menu");
-menuOpen.addEventListener("click", function () {
-  menu.classList.remove("hidden");
-  document.querySelector(".overlay").classList.remove("hidden");
-});
-menuClose.addEventListener("click", function () {
+const overlay = document.querySelector(".overlay");
+
+function close() {
   menu.classList.add("hidden");
-  document.querySelector(".overlay").classList.add("hidden");
+  overlay.classList.add("hidden");
+  menuOpen.src = "assets/images/icon-menu.svg";
+}
+
+menuOpen.addEventListener("click", function () {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+    menuOpen.src = "assets/images/icon-menu-close.svg";
+  } else close();
 });
+
+overlay.addEventListener("click", close);
